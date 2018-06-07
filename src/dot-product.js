aabc73477a906d11e64bdeb646ee1a8d1d3c28e3
Pablo Molina <contact@pablomolina.me>
2018-06-07 15:56:59 +0200

    Update docs

--- src/dot-product.js
+++ src/dot-product.js
@@ -1,6 +1,13 @@
 import haveSameNumberOfDimensions from "./assertions/have-same-number-of-dimensions";
 import isVector from "./assertions/is-vector";
 
+/**
+ * Calculates the dot product of two vectors
+ * @param {Vector} a
+ * @param {Vector} b
+ * @return {number}
+ * @see https://en.wikipedia.org/wiki/Dot_product
+ */
 function dotProduct(a, b) {
   isVector(a);
   isVector(b);
