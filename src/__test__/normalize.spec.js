bac5e567e77a3fd62ff8eaebaa5fb9a4e1cf5742
Pablo Molina <contact@pablomolina.me>
2018-06-13 15:14:58 +0200

    Added tests

--- src/__test__/normalize.spec.js
+++ src/__test__/normalize.spec.js
@@ -1,6 +1,8 @@
 import normalize from "../normalize";
 import Vector from "../vector";
 
+const EPSILON = Math.pow(2, -52);
+
 describe("normalize()", () => {
   it("needs a Vector", () => {
     expect(() => normalize(null)).toThrow(TypeError);
@@ -17,9 +19,21 @@ describe("normalize()", () => {
     expect(normalize(new Vector(10, 0)).length).toBe(1);
   });
 
-  it("preserves old vectors angle", () => {
-    const v = new Vector(16, 32, 1);
+  it("works for n-dimensional vectors", () => {
+    const hasLength1 = (...components) => {
+      const length = normalize(new Vector(...components)).length;
+      expect(1 - length <= EPSILON).toBe(true);
+    };
 
+    hasLength1(12);
+    hasLength1(-4, 8);
+    hasLength1(0, 43, -2);
+    hasLength1(-23.1, 14.44, 1, 0);
+    hasLength1(Math.PI, -Math.E, 0, 0, 1, 1);
+  });
+
+  it("preserves old vectors angle", () => {
+    const v = new Vector(16, 32);
     expect(normalize(v).angle).toBe(v.angle);
   });
 });
